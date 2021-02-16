4ce3b885cad50edda146c0752beb4ff46b795884
Catalin-cmd <63240047+Catalin-cmd@users.noreply.github.com>
2021-02-16 22:27:08 +0200

    Update Cisco_Ex_Sortare_Lista.py

--- Cisco_Ex_Sortare_Lista.py
+++ Cisco_Ex_Sortare_Lista.py
@@ -4,7 +4,7 @@ lista = [123,1243,3453,45776,2342,456456,32423,534634,4523,634,2356,345,3456,436
 invers = True
 while invers:
     invers = False
-    for i in range(len(lista)-1): #ia toata lungilea listei, intrucat len numara inapand de la 1
+    for i in range(len(lista)-1): #ia toata lungimea listei, intrucat len() numara inapand de la 1
         if lista[i]>lista[i+1]: #verifica daca primul element din lista este mai mare decat urmatorul
             invers = True #devine adevarat, astfel incat va rula inca o data
             lista[i], lista[i+1]=lista[i+1], lista[i] #se face switch-ul intre primul element si al doilea
