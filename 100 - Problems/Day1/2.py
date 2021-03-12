d7a59457e74123ba0e7507e4adb1435061ef3e2e
Catalin Cristea <63240047+catacristea@users.noreply.github.com>
2021-03-12 16:06:54 +0200

    100 Problems
    Solving 3 problems per day

---
+++ 100 - Problems/Day1/2.py
@@ -0,0 +1,14 @@
+"""
+Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma-separated sequence on a single line.
+Suppose the following input is supplied to the program: 8 Then, the output should be:40320
+"""
+def factorial(num):
+    d = 1
+    for i in range(num):
+        d*=i+1
+        print(d, end=', ')
+    print(f'\n!{num} = {d}')
+
+n = input('Intodu un numar: ')
+factorial(int(n))
