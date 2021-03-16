cc50bbcc9e273fbe0a8cbb145abaf6a667709922
Catalin Cristea <63240047+catacristea@users.noreply.github.com>
2021-03-16 12:51:30 +0200

    Day4

---
+++ 100 - Problems/Day4/2.py
@@ -0,0 +1,19 @@
+'''
+Question 15:
+
+    Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+
+    Suppose the following input is supplied to the program:
+
+9
+
+    Then, the output should be:
+
+11106
+
+'''
+total=0
+a = input('s')
+for i in range(1,5):
+    total+=int(i*a) #at first iteration, result will be a, at second one, result will be aa, we multiply a string by i times
+print(total)
